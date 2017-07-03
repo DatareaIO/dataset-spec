1dacd536fc4890f3f5805a5f9a1e12d3be72c13e
haoliangyu <njkon@foxmail.com>
2017-07-03 15:05:10 -0400

    remove namespace

--- index.d.ts
+++ index.d.ts
@@ -1,141 +1,137 @@
 import { MultiPolygon } from '@types/geojson';
 
-declare namespace datarea {
+/**
+ * Definition for dataset file
+ */
+export interface DatasetFile {
 
   /**
-   * Definition for dataset file
+   * file name
+   * @type {String}
    */
-  export interface DatasetFile {
-
-    /**
-     * file name
-     * @type {String}
-     */
-    name: String;
-
-    /**
-     * file url
-     * @type {String}
-     */
-    url: String
-
-    /**
-     * file format (optional)
-     * @type  {String}
-     */
-    format?: String;
-
-    /**
-     * file description (optional)
-     * @type {String}
-     */
-    description?: String;
-
-    /**
-     * file extension without dot (optional)
-     * @type {String}
-     */
-    extension?: String;
-  }
+  name: String;
 
   /**
-   * Definition for dataset
+   * file url
+   * @type {String}
    */
-  export interface Dataset {
+  url: String
 
-    /**
-     * dataset name
-     * @type {String}
-     */
-    name: String;
+  /**
+   * file format (optional)
+   * @type  {String}
+   */
+  format?: String;
 
-    /**
-     * universal unique id for this dataset in the portal
-     */
-    uuid: String;
+  /**
+   * file description (optional)
+   * @type {String}
+   */
+  description?: String;
 
-    /**
-     * data portal name
-     * @type {String}
-     */
-    portal: String;
+  /**
+   * file extension without dot (optional)
+   * @type {String}
+   */
+  extension?: String;
+}
 
-    /**
-     * dateset updated time
-     * @type {Date}
-     */
-    updated: Date;
+/**
+ * Definition for dataset
+ */
+export interface Dataset {
 
-    /**
-     * dataset url on the portal
-     * @type {String}
-     */
-    url: String;
+  /**
+   * dataset name
+   * @type {String}
+   */
+  name: String;
 
-    /**
-     * dataset publisher
-     * @type {String}
-     */
-    publisher: String;
+  /**
+   * universal unique id for this dataset in the portal
+   */
+  uuid: String;
 
-    /**
-     * dataset tags
-     * @type {Array<String>}
-     */
-    tags: Array<String>;
+  /**
+   * data portal name
+   * @type {String}
+   */
+  portal: String;
 
-    /**
-     * dataset categories
-     * @type {Array<String>}
-     */
-    categories: Array<String>;
+  /**
+   * dateset updated time
+   * @type {Date}
+   */
+  updated: Date;
 
-    /**
-     * dataset files
-     * @type {Array<DatasetFile>}
-     */
-    files: Array<DatasetFile>;
+  /**
+   * dataset url on the portal
+   * @type {String}
+   */
+  url: String;
 
-    /**
-     * original dataset metadata
-     * @type {Object}
-     */
-    raw: Object;
+  /**
+   * dataset publisher
+   * @type {String}
+   */
+  publisher: String;
 
-    /**
-     * version number of current metadata
-     */
-    version: Number;
+  /**
+   * dataset tags
+   * @type {Array<String>}
+   */
+  tags: Array<String>;
 
-    /**
-     * dataset created time (optional)
-     * @type {Date}
-     */
-    created?: Date;
+  /**
+   * dataset categories
+   * @type {Array<String>}
+   */
+  categories: Array<String>;
 
-    /**
-     * dataset description (optional)
-     * @type {String}
-     */
-    description?: String;
+  /**
+   * dataset files
+   * @type {Array<DatasetFile>}
+   */
+  files: Array<DatasetFile>;
 
-    /**
-     * dataset publication license (optional)
-     * @type {String}
-     */
-    license?: String;
+  /**
+   * original dataset metadata
+   * @type {Object}
+   */
+  raw: Object;
 
-    /**
-     * data portal platform name
-     * @type {String}
-     */
-    platform?: String;
+  /**
+   * version number of current metadata
+   */
+  version: Number;
 
-    /**
-     * dataset coverage region (optional)
-     * @type {MultiPolygon}
-     */
-    region?: MultiPolygon;
-  }
+  /**
+   * dataset created time (optional)
+   * @type {Date}
+   */
+  created?: Date;
 
+  /**
+   * dataset description (optional)
+   * @type {String}
+   */
+  description?: String;
+
+  /**
+   * dataset publication license (optional)
+   * @type {String}
+   */
+  license?: String;
+
+  /**
+   * data portal platform name
+   * @type {String}
+   */
+  platform?: String;
+
+  /**
+   * dataset coverage region (optional)
+   * @type {MultiPolygon}
+   */
+  region?: MultiPolygon;
 }
